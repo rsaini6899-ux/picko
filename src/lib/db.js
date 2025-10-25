d935d028c4e56daaa63d8abb4a65e50b5462c500
Ravi Saini <rsaini6899@gmail.com>
2025-10-25 13:21:51 +0530

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export async function connect() {
     try {
-      await mongoose.connect(process.env.MONGO_URI); // ✅ No need for extra options
+      await mongoose.connect(process.env.MONGODB_URI); // ✅ No need for extra options
   
       const connection = mongoose.connection;
   

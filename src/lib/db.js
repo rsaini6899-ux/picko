ee095baf617e39d1e8f97568eecd7eb990f67775
Ravi Saini <rsaini6899@gmail.com>
2025-11-06 10:58:45 +0530

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -20,31 +20,46 @@
 //   }
   
 
+// import mongoose from "mongoose";
+
+// const MONGODB_URI = process.env.MONGODB_URI;
+
+// if (!MONGODB_URI) {
+//   throw new Error("Please define the MONGODB_URI environment variable");
+// }
+
+// let cached = global.mongoose;
+
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
+
+// export async function connect() {
+
+//   if (cached.conn) return cached.conn;
+
+//   if (!cached.promise) {
+//     cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+//       return mongoose;
+//     });
+//   }
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
+
+
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable");
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-export async function connect() {
-
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-      return mongoose;
+export const connect = async () => {
+  try {
+    if (mongoose.connection.readyState === 1) return;
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "picko",
     });
+    console.log("✅ MongoDB connected successfully");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
   }
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+};
 
 
